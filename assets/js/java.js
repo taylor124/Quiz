7cc28053db2e2473db67b1a52bf89a3b63ec3664
taylor124 <74930599+taylor124@users.noreply.github.com>
2021-01-08 21:43:40 -0500

    Feature: Adding Event Listener to a button

--- assets/js/java.js
+++ assets/js/java.js
@@ -6,15 +6,4 @@
 //When the Player guesses the question incorrectly, reduce the time by 30 seconds and keep them on the same page.
 //When the Player guesses the question correctly, move on to the next question without penalty.
 //If the Player guesses it wrong 3 times, Game over.
-
-var timeLimit = 500;
-
-    function countdown(){
-        timeLimit--; // or totalTime = totalTime -1, or totalTime-=1;
-        console.log(timeLimit);
-        if(timeLimit<=0){
-            window.clearInterval(countdowntimer);
-        }
-    }
     
-    var countdowntimer = setInterval(countdown, 1000);
