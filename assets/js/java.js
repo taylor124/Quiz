9e8c8c5c426d6bf82e624cbbf3c3e47d02b5c6b8
taylor124 <74930599+taylor124@users.noreply.github.com>
2021-01-10 13:18:27 -0500

    Feature: localStorage working!

--- assets/js/java.js
+++ assets/js/java.js
@@ -7,38 +7,57 @@
 //When the Player guesses the question correctly, move on to the next question without penalty.
 //If the Player guesses it wrong 3 times, Game over.
 
-
-function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = minutes + ":" + seconds;
-
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
-}
-
+var display = document.querySelector('#time');
+var fiveMinutes = 60 * 5;
+var timer = fiveMinutes;
 window.onload = function () {
-    var fiveMinutes = 60 * 5,
-        display = document.querySelector('#time');
-    startTimer(fiveMinutes, display);
-};
-for (i = 0; i < 4; i++) {
-    document.getElementsByClassName('correct')[i].addEventListener('click', function () {
-        window.alert("Correct!");
-    })
+    startTimer();
 };
 
-for (i = 0; i < 12; i++) {
-    document.getElementsByClassName('false')[i].addEventListener('click', function () {
-        window.alert("incorrect");
-        
-    })
+
+var setIntervalfunction = setInterval(function () {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = minutes + ":" + seconds;
+
+    timer-- ;
+    console.log(timer)
+    if (timer === -1) {
+        clearInterval(setIntervalfunction);
+        window.alert("time is up");
+
+    };
+
+
+}, 1000);
+
+function startTimer() {
+
+    setIntervalfunction;
+
+
+    for (i = 0; i < 4; i++) {
+        document.getElementsByClassName('correct')[i].addEventListener('click', function () {
+            window.alert("Correct!");
+        })
+    };
+
+    for (i = 0; i < 12; i++) {
+        document.getElementsByClassName('false')[i].addEventListener('click', function () {
+            window.alert("incorrect");
+            timer -= 29;
+        })
+    };
 };
+
+
+
+
+
+
+
+
